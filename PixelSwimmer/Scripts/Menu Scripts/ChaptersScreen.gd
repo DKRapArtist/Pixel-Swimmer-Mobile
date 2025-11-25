1157ffc797aff2feef506d8e12e52c3fb11eaef4
DKRapArtist <167987183+DKRapArtist@users.noreply.github.com>
2025-11-25 21:37:07 +0200

    Changes Buttons

--- PixelSwimmer/Scripts/Menu Scripts/ChaptersScreen.gd
+++ PixelSwimmer/Scripts/Menu Scripts/ChaptersScreen.gd
@@ -1,15 +1,19 @@
 class_name ChaptersScreen
 extends Node2D
 
-@onready var chapter_2: TextureButton = $Buttons/Chapter2
-@onready var chapter_3: TextureButton = $Buttons/Chapter3
-@onready var chapter_4: TextureButton = $Buttons/Chapter4
+@onready var chapter_2 = $Buttons/Chapter2
+@onready var chapter_3 = $Buttons/Chapter3
+@onready var chapter_4 = $Buttons/Chapter4
 
 func _ready() -> void:
 	# Lock level 2 if highest_unlocked_level < 1
-	chapter_2.disabled = GameSession.highest_unlocked_chapter < 1
-	chapter_3.disabled = GameSession.highest_unlocked_chapter < 2
-	chapter_4.disabled = GameSession.highest_unlocked_chapter < 3
+	lock_touch_button(chapter_2, GameSession.highest_unlocked_chapter < 1)
+	lock_touch_button(chapter_3, GameSession.highest_unlocked_chapter < 2)
+	lock_touch_button(chapter_4, GameSession.highest_unlocked_chapter < 3)
+
+func lock_touch_button(btn: TouchScreenButton, locked: bool) -> void:
+	btn.set_block_signals(locked)
+	btn.get_node("LockedOverlay").visible = locked
 
 func _on_chapter_1_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Menu Scenes/Chapters/Chapter1.tscn")
